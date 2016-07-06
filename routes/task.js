a4665ad96b1010d8c054c4d20000a83eeed1afe1
Dan Taylor <dt.raelcun@gmail.com>
2016-07-05 21:17:28 -0400

    resolved conflicts

--- routes/task.js
+++ routes/task.js
@@ -1,6 +1,6 @@
 const express = require('express'),
-            Task = require('../models/task'),
-            router = express.Router();
+      Task = require('../models/task'),
+      router = express.Router();
 
 router.get('/tasks', function(req, res) {
 
@@ -31,6 +31,29 @@ router.put('/tasks/:id', function(req, res) {
     })
 });
 
+router.post('/tasks', function(req, res) {
+	
+	const task = new Task();
+	task.title = req.body.title;
+	task.priority = req.body.priority;
+	task.completed = req.body.completed;
+	if (task.completed_at) task.completed_at = req.body.completed_at;
+	if (task.due_at) task.due_at = req.body.due_at;
+	
+	task.save(function(err) {
+		if (err) return res.json({ success: false, error: { id: 1, message: "Error" } });
+			
+		return res.json({ success: true, results: task.id });
+	});
+})
+
+router.delete('/tasks/:taskid', function(req, res) {
+	Task.findByIdAndRemove(req.params.taskid, function(err, a, b) {
+		if (err) return res.json({ success: false, error: { id: 2, message: "Error" } })
+		
+		return res.json({ success: true })
+	});
+});
 
 module.exports = router;
 
