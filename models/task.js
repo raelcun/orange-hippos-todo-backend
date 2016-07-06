fb155d425a0ae6b6e5a766d6b3dbfa47ce7860c2
Michael Ho <michaelho3624@gmail.com>
2016-07-05 20:30:16 -0400

    Created first route for Task and created Task Schema for Mongoose.
    Error from trying to run.

---
+++ models/task.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var TaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        required: true
+    },
+    completed_at: {
+        type: String
+    },
+    due_at: {
+        type: String
+    }
+
+});
+
+module.exports = mongoose.model('Person', TaskSchema);
--- routes/task.js
+++ routes/task.js
@@ -2,3 +2,21 @@ const express = require('express'),
             Task = require('../models/task'),
             router = express.Router();
 
+router.get('/tasks', function(req, res) {
+
+    // returns all the tasks from the Database
+    Task.find({}).lean().exec(function(err, results) {
+
+        if (err) {
+            return res.json({ success: false, error: err });
+        }
+
+        return res.json({ success: true, results: results });
+
+    });
+
+});
+
+
+module.exports = router;
+
