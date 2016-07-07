c8afc466c7194b3132b4c18b371b33713fdc04b8
Dan Taylor <dt.raelcun@gmail.com>
2016-07-06 20:41:23 -0400

    Use describe-it style for mocha

--- test/integration/tasks.js
+++ test/integration/tasks.js
@@ -3,6 +3,12 @@ process.env.NODE_ENV = 'testing'
 const supertest = require('supertest'),
 			app = require('../../app.js')
 
-exports.test = function(done) {
-	supertest(app).get('/tasks').expect(200).end(done)
-}
+describe('integration tests', function() {
+	it('should get posted data', function(done) {
+		supertest(app).get('/tasks').expect(200).end(done)
+	})
+})
+
+// exports.should_get_posted_data = function(done) {
+// 	supertest(app)
+// }
